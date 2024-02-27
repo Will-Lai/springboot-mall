b4cf459fc93d6f7e6c20f7cfca8dad87ed6dc415
William Lai <waihtgu@gmail.com>
2024-02-28 00:51:37 +0800

    product item - query a single product item

---
+++ src/main/java/com/william/springbootmall/service/ProductService.java
@@ -0,0 +1,7 @@
+package com.william.springbootmall.service;
+
+import com.william.springbootmall.model.Product;
+
+public interface ProductService {
+    Product getProductById(Integer productId);
+}
---
+++ src/main/java/com/william/springbootmall/service/impl/ProductServiceImpl.java
@@ -0,0 +1,18 @@
+package com.william.springbootmall.service.impl;
+
+import com.william.springbootmall.dao.ProductDao;
+import com.william.springbootmall.model.Product;
+import com.william.springbootmall.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductDao productDao;
+    @Override
+    public Product getProductById(Integer productId) {
+        return productDao.getProductById(productId);
+    }
+}
