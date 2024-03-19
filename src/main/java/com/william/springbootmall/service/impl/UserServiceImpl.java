97b52b3d3d95063fd63c4a73e07bd8620468027f
William Lai <waihtgu@gmail.com>
2024-03-19 23:12:38 +0800

    user - check register duplicate email

--- src/main/java/com/william/springbootmall/service/impl/UserServiceImpl.java
+++ src/main/java/com/william/springbootmall/service/impl/UserServiceImpl.java
@@ -4,17 +4,31 @@ import com.william.springbootmall.dao.UserDao;
 import com.william.springbootmall.dto.UserRegisterRequest;
 import com.william.springbootmall.model.User;
 import com.william.springbootmall.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserServiceImpl implements UserService {
+
+    private final static Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Autowired
     private UserDao userDao;
 
     @Override
     public Integer register(UserRegisterRequest userRegisterRequest) {
+        // check duplicate email
+        User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
+
+        if (user != null) {
+            log.warn("duplicate email: {}", userRegisterRequest.getEmail());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
         return userDao.createUser(userRegisterRequest);
     }
 
